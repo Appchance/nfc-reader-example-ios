7f3fa765ac72864d3f5c589704bc008fb7207bb9
Michał Banaszyński <banaszynskimichal@gmail.com>
2018-01-26 13:00:33 +0100

    Initial Commit

---
+++ NFCReaderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFCReaderApp
+//
+//  Created by Michal Banaszynski on 26/01/2018.
+//  Copyright © 2018 Michal Banaszynski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
